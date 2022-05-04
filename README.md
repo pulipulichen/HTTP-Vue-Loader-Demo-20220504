410480d3ef9e9abf19f1fc839fd8be3056c8b4ab
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-05-04 17:48:31 +0800

    Wed May  4 05:48:31 PM CST 2022

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # HTTP-Vue-Loader-Demo-20220504
 
 https://dotblogs.com.tw/HelloDeveloper/2017/09/21/143929
+
+
+https://pulipulichen.github.io/HTTP-Vue-Loader-Demo-20220504/
--- my-com/my-com.vue
+++ my-com/my-com.vue
@@ -1,3 +1,4 @@
 <style src="my-com/my-com.css" scoped></style>
+<style src="my-com/my-com2.css" scoped></style>
 <template src="my-com/my-com.html"></template>
 <script src="my-com/my-com.js"></script>
